a2e22829d5d9d42cd76364fc5cdbd2b89dfe278d
wg4568 <wg4568@gmail.com>
2016-08-07 17:46:41 -0700

    Created a "Planet" class
    created a planet class that can generate and save an image from certain
    values. Defaults to the same defaults as origional planet_generator.py

--- planet_generator.py
+++ planet_generator.py
@@ -1,47 +1,10 @@
-import pygame, helpers, loadingbars
-from random import randint
-from PIL import Image, ImageFilter
+import pygame, planets
 
-img = Image.new('RGB', (1000, 1000))
-img = img.convert("RGBA")
+planet = planets.Planet()
+planet.generate_image()
+planet.save("image.png")
 
-noise = []
-size = 1000
-temp = 100
-seed = randint(0, 10000)
-color = helpers.random_color()
-ocean = helpers.random_color()
-water = 50
-sharp = 300.
-
-loadingbarsbar = loadingbars.LoadingBar(minval=0, maxval=size)
-
-for y in xrange(1, size+1):
-	loadingbarsbar.set(y)
-	for x in xrange(1, size+1):
-		if helpers.dist_between(x, y, *[size/2]*2) < size/2:
-			n = helpers.translate(helpers.noise2D(x, y, seed, sharp=sharp), -1, 1, 0, 1)
-			if n > water/100.:
-				r = int(n*color[0])
-				g = int(n*color[1])
-				b = int(n*color[2])
-			else:
-				r = int(n*ocean[0])
-				g = int(n*ocean[1])
-				b = int(n*ocean[2])
-			noise.append((r, g, b))
-		else:
-			noise.append((0, 0, 0, 0))
-
-x = ImageFilter.GaussianBlur(radius=size/100)
-
-img.putdata(noise)
-blurred_image = img.filter(x)
-blurred_image.save('image.png')
-
-print color, ocean, water, sharp, seed
-
-planet = pygame.transform.scale(pygame.image.load("image.png"), [500, 500])
+planet_img = pygame.transform.scale(pygame.image.load("image.png"), [500, 500])
 
 class Game:
 	def __init__(self):
@@ -63,7 +26,7 @@ class Game:
 		pass
 
 	def draw(self):
-		self.screen.blit(planet, [0, 0])
+		self.screen.blit(planet_img, [0, 0])
 
 	def start(self):
 		self.running = True
--- planets.py
+++ planets.py
@@ -1,3 +1,43 @@
+import pygame, helpers, loadingbars
+from random import randint
+from PIL import Image, ImageFilter
+
 class Planet:
-	def __init__(self, seed="planet"):
+	def __init__(self, seed="planet", size=1000, sharp=100, land_color=helpers.random_color(), ocean_color=helpers.random_color(), water=50, blur=10):
 		self.seed = seed
+		self.size = size
+		self.sharp = float(sharp)
+		self.sharp_img = Image.new('RGB', (1000, 1000)).convert("RGBA")
+		self.land_color = land_color
+		self.ocean_color = ocean_color
+		self.water = water
+		self.blur = blur
+		self.blur_filter = ImageFilter.GaussianBlur(radius=self.blur)
+
+	def generate_image(self):
+		noise = []
+		loadingbar = loadingbars.LoadingBar(minval=0, maxval=self.size)
+		for y in xrange(1, self.size+1):
+			loadingbar.set(y)
+			for x in xrange(1, self.size+1):
+				if helpers.dist_between(x, y, *[self.size/2]*2) < self.size/2:
+					n = helpers.translate(helpers.noise2D(x, y, self.seed, sharp=self.sharp), -1, 1, 0, 1)
+					if n > self.water/100.:
+						r = int(n*self.land_color[0])
+						g = int(n*self.land_color[1])
+						b = int(n*self.land_color[2])
+					else:
+						r = int(n*self.ocean_color[0])
+						g = int(n*self.ocean_color[1])
+						b = int(n*self.ocean_color[2])
+					noise.append((r, g, b))
+				else:
+					noise.append((0, 0, 0, 0))
+		self.sharp_img.putdata(noise)
+		self.blurred_img = self.sharp_img.filter(self.blur_filter)
+
+	def save(self, fname, blurred=True):
+		if blurred:
+			self.blurred_img.save(fname)
+		else:
+			self.sharp_img.save(fname)
